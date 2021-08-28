1a1bf503d6ed1bdca6a219790ab68e3434f4f3f2
Bartkk0 <32180149+Bartkk0@users.noreply.github.com>
2021-08-28 20:53:48 +0200

    Remove unnecesary patch

--- AVATAR_2021.08.11.15.16/patch.sh
+++ AVATAR_2021.08.11.15.16/patch.sh
@@ -10,5 +10,4 @@ dos2unix Dependencies/VRChat/Editor/ControlPanel/VRCSdkControlPanelAccount.cs
 
 patch -p1 < "$PATCHES/patch.diff"
 
-xdelta3 -f -d -s Plugins/VRCSDKBase-Editor.dll "$PATCHES/VRCSDKBase-Editor.vcdiff" Plugins/VRCSDKBase-Editor.dll
 xdelta3 -f -d -s Plugins/VRCSDK3A-Editor.dll "$PATCHES/VRCSDK3A-Editor.vcdiff" Plugins/VRCSDK3A-Editor.dll
