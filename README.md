b1ce215ba37f66d1daf3090f75ded9c6d897523b
Bartkk0 <thebartkk@gmail.com>
2022-06-25 04:05:06 +0200

    First version

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# VRChat SDK Linux patch
+# VRChat SDK Linux patch (New and improved)
 
 ## Warning: By modifying the SDK you are violating the VRChat TOS. Do it at your own risk!
+
+Now using Harmony to avoid DLL patches.
+Currently only avatar SDK works.
+
+1. Import SDK.
+2. Import patch.
+3. Select steamapps folder location in SDK settings.
+4. Profit.
+
+## Development
+
+Download Harmony .NET 4.7 into editor folder.
---
+++ SdkPatcher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using HarmonyLib;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDKBase.Editor;
+
+[InitializeOnLoad]
+public static class SdkPatcher {
+    private static Harmony _harmony;
+    public static string _steamPath;
+
+    static SdkPatcher() {
+        if (_harmony == null) {
+            _harmony = new Harmony("pl.barkk.vrcsdk_linux");
+        }
+        
+        _steamPath = "";
+        if (EditorPrefs.HasKey("VRC_steamappsPath"))
+            _steamPath = EditorPrefs.GetString("VRC_steamappsPath");
+        _harmony.UnpatchAll();
+        _harmony.PatchAll();
+    }
+}
+ 
+[HarmonyPatch(typeof(VRC_SdkBuilder))]
+[HarmonyPatch(nameof(VRC_SdkBuilder.GetKnownFolderPath))]
+class EditorPatch {
+    static bool Prefix(ref string __result, Guid knownFolderId) {
+        Debug.Log(knownFolderId);
+        if (knownFolderId.ToString() == "a520a1a4-1780-4ff6-bd18-167343c5af16") {
+            __result = EditorPrefs.GetString("VRC_steamappsPath") +
+                       "/compatdata/438100/pfx/drive_c/users/steamuser/AppData/LocalLow/";
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(VRCSdkControlPanel))]
+[HarmonyPatch("OnVRCInstallPathGUI")]
+class ControlPanelPatch {
+    static void Prefix() {
+        EditorGUILayout.LabelField("steamapps directory", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("steamapps: ", SdkPatcher._steamPath);
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("");
+        if (GUILayout.Button("Edit")) {
+            string initPath = "";
+            if (!string.IsNullOrEmpty(SdkPatcher._steamPath))
+                initPath = SdkPatcher._steamPath;
+
+            SdkPatcher._steamPath = EditorUtility.OpenFolderPanel("Choose steamapps directory", initPath, "");
+
+            EditorPrefs.SetString("VRC_steamappsPath", SdkPatcher._steamPath);
+            // window.OnConfigurationChanged();
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+}
